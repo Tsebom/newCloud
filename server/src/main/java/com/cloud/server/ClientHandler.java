d88d67189f6876e84b0ea54af25dbc30c0d87ec6
Nezhdanov Sergei <tsebom@yandex.ru>
2021-07-05 19:32:34 +0300

    empty_commit

---
+++ server/src/main/java/com/cloud/server/ClientHandler.java
@@ -0,0 +1,9 @@
+package com.cloud.server;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+
+public class ClientHandler {
+    public ClientHandler(SelectionKey key, Selector selector) {
+    }
+}
---
+++ server/src/main/java/com/cloud/server/HandlAccept.java
@@ -0,0 +1,9 @@
+package com.cloud.server;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+
+public class HandlAccept {
+    public HandlAccept(SelectionKey key, Selector selector) {
+    }
+}
